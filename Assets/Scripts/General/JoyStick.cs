6ea175632df9e3890a421463519922de98b60802
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-03-09 18:06:36 +0800

    Started on Joystick

---
+++ Assets/Scripts/General/JoyStick.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStick : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public static JoyStick Instance = null;
+    private bool pressed = false;
+    private int touchIndex = 0;
+
+    private Vector2 firstTouch = new Vector2();
+    private Vector2 touchOffset = new Vector2();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if(pressed)
+        {
+            touchOffset = Input.GetTouch(touchIndex).position / new Vector2(Camera.main.scaledPixelWidth, Camera.main.scaledPixelHeight);
+            touchOffset -= firstTouch;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!pressed)
+        {
+            touchIndex = Input.touchCount - 1;
+            firstTouch = Input.GetTouch(touchIndex).position / new Vector2(Camera.main.scaledPixelWidth, Camera.main.scaledPixelHeight);
+        }
+
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+        touchOffset = Vector2.zero;
+    }
+
+    public Vector2 GetTouchOffset()
+    {
+        return touchOffset;
+    }
+}
---
+++ Assets/Scripts/Mechanics/Player/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float jumpForce = 10f;
+
+    private void Update()
+    {
+        Debug.Log(JoyStick.Instance.GetTouchOffset());
+    }
+}

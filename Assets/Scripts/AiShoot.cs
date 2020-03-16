caf185d48b5824a3adfb89247e9dc24495b973f3
eugeneyj <42328890+eugeneyj@users.noreply.github.com>
2020-03-16 15:50:04 +0800

    kean
    gay

---
+++ Assets/Scripts/AiShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float bulletSpeed;
+   
+
+    public AiPatrol AiDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AiDirection = FindObjectOfType<AiPatrol>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector2.right * bulletSpeed * Time.deltaTime);
+    }
+}

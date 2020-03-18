eeeace7d9afd606393133ffb96bc8cf8dd3fe5f8
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-03-18 12:16:11 +0800

    Added Player Movement

---
+++ Assets/Scripts/Gestures/Gesture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Gesture : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [SerializeField] private float minDrag = 0f;
+
+    public static Vector2 lastSwipe = Vector2.zero;
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        lastSwipe = eventData.position - eventData.pressPosition;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    { 
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Joystick input = null;
+
+    [Header("Stats")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+
+    private Rigidbody2D rb = null;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 targetPos = transform.position;
+
+        targetPos.x += input.Horizontal * moveSpeed * Time.deltaTime;
+
+        if (Gesture.lastSwipe.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        transform.position = targetPos;
+    }
+
+    private void LateUpdate()
+    {
+        Gesture.lastSwipe = Vector2.zero;
+    }
+}

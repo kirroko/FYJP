9e43d6318dbcb8f854aa5a277cb6b1a46eb59f04
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-03-19 17:34:40 +0800

    Created Base Isometric tilemap

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -17,45 +17,29 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float minJumpDrag = 10f;
 
     private Rigidbody2D rb = null;
-    private Rigidbody rb3D = null;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb3D = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        //Vector3 targetPos = transform.position;
+        Vector3 targetPos = transform.position;
 
-        //targetPos.x += input.Horizontal * moveSpeed * Time.deltaTime;
-        ////For 3D
-        //targetPos.z += input.Vertical * moveSpeed * Time.deltaTime;
+        targetPos.x += input.Horizontal * moveSpeed * Time.deltaTime;
+        targetPos.y += input.Vertical * moveSpeed * Time.deltaTime;
 
-        //if (Gesture.lastSwipe.y > minJumpDrag)
-        //{
-        //    rb3D.velocity = new Vector2(rb3D.velocity.x, jumpForce);
-        //}
-
-        //if (input.Horizontal < 0f)
-        //    transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-        //else if( input.Horizontal > 0f)
-        //    transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-
-        //transform.position = targetPos;
         if (Gesture.lastSwipe.y > minJumpDrag)
         {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-    }
 
-    private void FixedUpdate()
-    {
-        Vector3 targetVelocity = new Vector2(input.Horizontal * moveSpeed, rb.velocity.y);
-        Vector3 temp = Vector3.zero;
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref temp, dampForce);
+        if (input.Horizontal < 0f)
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        else if (input.Horizontal > 0f)
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
 
-
+        transform.position = targetPos;
     }
 }

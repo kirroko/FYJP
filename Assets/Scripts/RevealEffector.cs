543033c171de2ec6fe6989f553d56c726063015e
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-03-22 19:20:02 +0800

    Added Shitty Outline Shader

--- Assets/Scripts/RevealEffector.cs
+++ Assets/Scripts/RevealEffector.cs
@@ -4,39 +4,46 @@ using UnityEngine;
 
 public class RevealEffector : MonoBehaviour
 {
-    public Transform effector = null;
-
     void Update()
     {
-        //if(Input.GetMouseButtonDown(0))
-        //{
-        //    Vector3 pos = Input.mousePosition;
-        //    Debug.Log("MousePos: " + pos + " TouchPos: " + Input.GetTouch(0).position);
-        //    pos.z = 10f;
-        //    Debug.Log(Camera.main.ScreenToWorldPoint(pos));
-        //}
-
-
-        if (Input.touchCount > 0)
+        if (Input.GetMouseButton(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit[] results = Physics.RaycastAll(ray);
             foreach (RaycastHit result in results)
             {
-                if(result.collider.GetComponent<RevealEffector>() != null)
+                if (result.collider.GetComponent<RevealEffector>() != null)
                 {
                     GetComponent<Renderer>().sharedMaterial.SetVector("_Position", result.point);
                 }
             }
-            //Vector3 pos = Input.GetTouch(0).position;
-            //pos.z = Mathf.Abs(transform.position.z - Camera.main.transform.position.z);
-            //Vector3 touchPos = Camera.main.ScreenToWorldPoint(pos);
-            //Debug.Log(touchPos);
-            //touchPos.z = transform.position.z;
-            //GetComponent<Renderer>().sharedMaterial.SetVector("_Position", touchPos);
         }
         else
-            GetComponent<Renderer>().sharedMaterial.SetVector("_Position", effector.position);
+        {
+            GetComponent<Renderer>().sharedMaterial.SetVector("_Position", new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity));
+        }
+
+
+        //if (Input.touchCount > 0)
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        //    RaycastHit[] results = Physics.RaycastAll(ray);
+        //    foreach (RaycastHit result in results)
+        //    {
+        //        if(result.collider.GetComponent<RevealEffector>() != null)
+        //        {
+        //            GetComponent<Renderer>().sharedMaterial.SetVector("_Position", result.point);
+        //        }
+        //    }
+        //    //Vector3 pos = Input.GetTouch(0).position;
+        //    //pos.z = Mathf.Abs(transform.position.z - Camera.main.transform.position.z);
+        //    //Vector3 touchPos = Camera.main.ScreenToWorldPoint(pos);
+        //    //Debug.Log(touchPos);
+        //    //touchPos.z = transform.position.z;
+        //    //GetComponent<Renderer>().sharedMaterial.SetVector("_Position", touchPos);
+        //}
+        //else
+        //    GetComponent<Renderer>().sharedMaterial.SetVector("_Position", effector.position);
 
     }
 }

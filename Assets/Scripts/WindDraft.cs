c3de4c15edeb073d3b1962bbd3793168e2d832ec
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-03-30 15:45:27 +0800

    Added WindDraft

---
+++ Assets/Scripts/WindDraft.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WindDraft : MonoBehaviour
+{
+    [SerializeField] private Vector2 direction = new Vector2(1f, 0f);
+    [SerializeField] private float force = 10f;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+
+        if (rb == null) return;
+
+        rb.velocity += direction * force;
+    }
+}

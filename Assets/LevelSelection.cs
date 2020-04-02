2a2910d49e2f57e6a1dd8ae7a55d947331d1daa6
eugeneyj <42328890+eugeneyj@users.noreply.github.com>
2020-04-02 13:20:13 +0800

    level selection
    level selection

---
+++ Assets/LevelSelection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    private float placeHolder;
+    public int LevelIndex;
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        
+        if(collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Player in range");
+            if (Input.GetKey(KeyCode.E))
+            {
+                placeHolder += Time.deltaTime * 0.5f;  
+                Debug.Log(placeHolder);
+                Debug.Log(LevelIndex);
+                if (placeHolder >= 1.0)
+                    Debug.Log("nextLevel");
+               //   SceneManager.LoadScene(Level1);
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+}

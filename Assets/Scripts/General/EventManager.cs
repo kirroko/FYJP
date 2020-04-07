e85d57f447fed80bc2238c97164b819a6c8d6398
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-04-07 10:42:05 +0800

    Added Platform Color

---
+++ Assets/Scripts/General/EventManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager instance = null;
+
+    public event Action<COLORS> PlatformColorEvent = null;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(instance);
+    }
+
+    public void TriggerPlatformColorEvent(COLORS playerColor)
+    {
+        if (PlatformColorEvent != null)
+            PlatformColorEvent(playerColor);
+    }
+
+}

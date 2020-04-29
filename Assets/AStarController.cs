c7a11554143c109ca1ecb7ea64f613af420bf449
eugeneyj <42328890+eugeneyj@users.noreply.github.com>
2020-04-29 18:19:01 +0800

    AI Controller
    Ai* controller!

---
+++ Assets/AStarController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AStarController : AI
+{
+    private AIPath enemy;
+
+  
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        enemy = gameObject.GetComponent<AIPath>();
+
+        
+    }
+    protected override void Update()
+    {
+        base.Update();
+
+        if (stun)
+            enemy.canMove = false;
+        else
+            enemy.canMove = true;
+
+        if (frozen)
+            enemy.maxSpeed = moveSpeed * slowAmt;
+        else
+            enemy.maxSpeed = moveSpeed;
+    }
+  
+}

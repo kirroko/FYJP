c5b593340c45f1a92d545ff559d956cace3d2b1b
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-04-30 12:50:26 +0800

    Update Mainmenu and Setting Sprites

--- Assets/Scripts/General/Mainmenu/Settings.cs
+++ Assets/Scripts/General/Mainmenu/Settings.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject main = null;
+    [SerializeField] private Button toggleMusicBtn = null;
+    [SerializeField] private Button toggleSFXBtn = null;
+
+    private void Start()
+    {
+        SwapSprites swapSpritesBGM = toggleMusicBtn.GetComponent<SwapSprites>();
+        swapSpritesBGM.Swap(AudioManager.instance.HasBGM);
+        toggleMusicBtn.onClick.AddListener(delegate () { swapSpritesBGM.Swap(AudioManager.instance.HasBGM); });
+
+        SwapSprites swapSpritesSFX = toggleSFXBtn.GetComponent<SwapSprites>();
+        swapSpritesSFX.Swap(AudioManager.instance.HasSFX);
+        toggleSFXBtn.onClick.AddListener(delegate () { swapSpritesSFX.Swap(AudioManager.instance.HasSFX); });
+    }
 
     public void ToMain()
     {

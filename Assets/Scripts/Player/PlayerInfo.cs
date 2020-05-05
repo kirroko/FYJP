2e1c378141e3d73eb96793101736030b3ca183c7
Kirroko <keanwng@gmail.com>
2020-05-05 17:50:36 +0800

    Death animation | Blinking Hurt | Reload level after death

--- Assets/Scripts/Player/PlayerInfo.cs
+++ Assets/Scripts/Player/PlayerInfo.cs
@@ -19,6 +19,7 @@ public class PlayerInfo : MonoBehaviour
 
     [SerializeField] private int heart = 1;
     [SerializeField] private float invincibleDuration = 3f;
+    [SerializeField] private GameObject canvas = null;
 
     private bool isInvincible = false;
 
@@ -27,10 +28,18 @@ public class PlayerInfo : MonoBehaviour
 
     private float invincibleInterval = 0f;
 
+    private Material whiteMat;
+    private Material defaultMat;
+    private SpriteRenderer sr;
+
     private void Start()
     {
         movement = GetComponent<PlayerMovement>();
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        
+        whiteMat = Resources.Load("Material/Effect/WhiteShade", typeof(Material)) as Material;
+        defaultMat = sr.material;
     }
 
     private void Update()
@@ -47,18 +56,35 @@ public class PlayerInfo : MonoBehaviour
 
         heart -= damage;
         isInvincible = true;
-        invincibleInterval = 1f;
+        invincibleInterval = 1.0f;
 
         rb.AddForce(new Vector2(-movement.GetLastXDir, 1f) * knockbackForce, ForceMode2D.Impulse);
 
+        sr.material = whiteMat;
 
         if (heart <= 0)
         {
             GetComponent<Animator>().SetTrigger("Death");
-            // LevelManager.instance.RestartLevel();
+            Invoke("ResetMaterial", .15f);
+            canvas.SetActive(false);
+            StartCoroutine(DelayRestartLevel(1.0f));
         }
         else
+        {
             GetComponent<Animator>().SetTrigger("Hurt");
+            Invoke("ResetMaterial", .15f);
+        }
+    }
+
+    private void ResetMaterial()
+    {
+        sr.material = defaultMat;
+    }
+
+    private IEnumerator DelayRestartLevel(float time)
+    {
+        yield return new WaitForSeconds(time);
+        LevelManager.instance.RestartLevel();
     }
 
     public void GainHeart(int amt)

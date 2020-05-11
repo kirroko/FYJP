434a87812c6332100b6e8ca621b1bbbaeac3ffb4
Kirroko <keanwng@gmail.com>
2020-05-11 15:12:43 +0800

    Load screen added

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loading : MonoBehaviour
+{
+    private void Start()
+    {
+        EventManager.instance.startSceneTransition -= TriggerTransition;
+        EventManager.instance.startSceneTransition += TriggerTransition;
+
+        EventManager.instance.offSceneTransition -= TriggerTransitionOff;
+        EventManager.instance.offSceneTransition += TriggerTransitionOff;
+
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.instance.startSceneTransition -= TriggerTransition;
+        EventManager.instance.offSceneTransition -= TriggerTransitionOff;
+    }
+
+    private void TriggerTransition()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    private void TriggerTransitionOff()
+    {
+        this.gameObject.SetActive(false);
+    }    
+}

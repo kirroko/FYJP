852c8b9b8ffe8618d7554891e0fe44ed0c2c9efc
eugeneyj <42328890+eugeneyj@users.noreply.github.com>
2020-05-14 17:42:39 +0800

    fish jumps and layout

--- Assets/fishjumping.cs
+++ Assets/fishjumping.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fishjumping : MonoBehaviour
+public class fishjumping : AI
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private float thrust = 1.0f;
+    [SerializeField] private float timer = 3.0f;
+
+    public Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer > 0.0f)
+            rb.AddForce(transform.up * thrust);
+        if (timer <= -2.0f)
+            timer = 2.0f;
+    }
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+
     }
 }

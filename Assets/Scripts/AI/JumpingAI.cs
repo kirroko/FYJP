455e4e422b2522a40f2ee4cf5b10cf5814d77425
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-05-15 13:17:49 +0800

    Updated Jumping AI to only jump up and down

--- Assets/Scripts/AI/JumpingAI.cs
+++ Assets/Scripts/AI/JumpingAI.cs
@@ -4,54 +4,30 @@ using UnityEngine;
 
 public class JumpingAI : AI
 {
-    [SerializeField] private Vector2 direction = Vector2.up;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float jumpInterval = 3f;
 
+    private Rigidbody2D rb = null;
     private float jumpTime = 0f;
-    private Vector3 startPos = Vector3.zero;
-    private Vector3 targetPos = Vector3.zero;
-
-    private bool jump = false;
 
     protected override void Start()
     {
         base.Start();
 
+        rb = GetComponent<Rigidbody2D>();
         jumpTime = jumpInterval;
-        direction.Normalize();
-
-        startPos = transform.position;
-
-        Vector3 temp = direction * jumpForce;
-        targetPos = startPos + temp;
     }
 
-    // Update is called once per frame
     protected override void Update()
     {
         base.Update();
 
-        //Fish is at start Pos
-        float dist = Mathf.Abs((transform.position - startPos).magnitude);
-        if (dist <= 0.1f)
-            jumpTime -= Time.deltaTime;
+        jumpTime -= Time.deltaTime;
 
-        //Fish is jumping out
         if(jumpTime <= 0f)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-            //transform.position = Vector3.Lerp(transform.position, targetPos, speed * Time.deltaTime);
-
-            dist = Mathf.Abs((transform.position - targetPos).magnitude);
-
-            if (dist <= 0.1f)
-                jumpTime = jumpInterval;
-        }
-        else//Fish falling back down
-        {
-            transform.position = Vector3.MoveTowards(transform.position, startPos, speed * Time.deltaTime);
-            //transform.position = Vector3.Lerp(transform.position, startPos, speed * Time.deltaTime);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpTime = jumpInterval;
         }
     }
 }

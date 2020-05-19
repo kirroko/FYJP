ea45273c36f18a1dd92d18fd94d0beeefa33af92
Kirroko <keanwng@gmail.com>
2020-05-19 16:57:30 +0800

    Cutscene Impletemented

---
+++ Assets/Scripts/CutSceneManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class CutSceneManager : MonoBehaviour
+{
+    // References
+    private VideoPlayer videoPlayer;
+    private AudioSource audioSource;
+    private Animator ani;
+    private RawImage rImage;
+
+    private void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        audioSource = GetComponent<AudioSource>();
+        ani = GetComponent<Animator>();
+        rImage = GetComponent<RawImage>();
+        rImage.enabled = true;
+        StartCoroutine(RunVideo());
+    }
+
+    private IEnumerator RunVideo()
+    {
+        videoPlayer.Prepare();
+        while(!videoPlayer.isPrepared)
+        {
+            Debug.Log("Preparing Video");
+            yield return null;
+        }
+        Debug.Log("Video Prepared");
+
+        rImage.texture = videoPlayer.texture;
+
+        videoPlayer.Play();
+        audioSource.Play();
+
+        while(videoPlayer.isPlaying)
+        {
+            Debug.Log("Playback time: " + Mathf.FloorToInt((float)videoPlayer.time));
+            if (Mathf.FloorToInt((float)videoPlayer.time) == 28)
+               ani.SetTrigger("Fade");
+
+            if(Mathf.FloorToInt((float)videoPlayer.time) > 28)
+            {
+                if(videoPlayer.GetDirectAudioVolume(0) != 0)
+                {
+                    float temp = videoPlayer.GetDirectAudioVolume(0) - Time.deltaTime;
+                    videoPlayer.SetDirectAudioVolume(0, temp);
+                }
+            }
+
+            yield return null;
+        }
+
+        Debug.Log("Video playback finish");
+        gameObject.SetActive(false);
+    }
+}

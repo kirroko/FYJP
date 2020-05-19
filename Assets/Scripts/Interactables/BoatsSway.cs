937779531a286ab1237ff256ee1aae340fe97239
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-05-19 21:36:06 +0800

    Updated Boat Swaying For Level 2

--- Assets/Scripts/Interactables/BoatsSway.cs
+++ Assets/Scripts/Interactables/BoatsSway.cs
@@ -4,22 +4,57 @@ using UnityEngine;
 
 public class BoatsSway : MonoBehaviour
 {
-    float originalY;
-   [SerializeField]private float floatStrength = 1; // You can change this in the Unity Editor to 
-                                    // change the range of y positions that are possible.
+    //[SerializeField] private float maxTilt = 25f;
 
-    void Start()
+    private float startX = 0f;
+    private Vector2 startCenterMass = Vector2.zero;
+    private Rigidbody2D rb = null;
+
+    private void Start()
     {
-        this.originalY = this.transform.position.y;
+        startX = transform.position.x;
+        rb = GetComponent<Rigidbody2D>();
+        startCenterMass = rb.centerOfMass;
     }
 
-    void Update()
+    private void Update()
     {
-        transform.position = new Vector3(transform.position.x,
-            originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
-            transform.position.z);
+        Vector3 targetPos = transform.position;
+        targetPos.x = startX;
+        transform.position = targetPos;
+
+        //rb.rotation = Mathf.Clamp(rb.rotation, -maxTilt, maxTilt);
+
+        float averageX = 0f;
+        List<ContactPoint2D> contactPoints = new List<ContactPoint2D>();
+        int count = rb.GetContacts(contactPoints);
+
+        if (count == 0)
+        {
+            rb.centerOfMass = startCenterMass;
+            return;
+        }
+
+        foreach (ContactPoint2D contactPoint in contactPoints)
+        {
+            averageX += contactPoint.otherCollider.gameObject.transform.localPosition.x;
+        }
+        averageX /= -count;
+
+        rb.centerOfMass = new Vector2(averageX, rb.centerOfMass.y);
+
     }
 
+    //private void LateUpdate()
+    //{
+    //    Vector3 rotation = transform.localEulerAngles;
 
+    //    if (rotation.z > 300f)
+    //        rotation.z = Mathf.Clamp(rotation.z, 360f - maxTilt, 360f);
+    //    else if (rotation.z < 100f)
+    //        rotation.z = Mathf.Clamp(rotation.z, 0f, maxTilt);
+
+    //    transform.localRotation = Quaternion.Euler(rotation);
+    //}
 }
 

55efb6bc0e6813d11882ac326eb19004778b4ac7
Edward095 <42329335+Edward095@users.noreply.github.com>
2020-05-24 17:08:44 +0800

    Updated Platform Color and Moving Platform

--- Assets/Scripts/Interactables/Platforms/PlatformColor.cs
+++ Assets/Scripts/Interactables/Platforms/PlatformColor.cs
@@ -10,6 +10,9 @@ public class PlatformColor : MonoBehaviour
     private SpriteRenderer spriteRenderer = null;
     private List<Collider2D> colliders = new List<Collider2D>();
 
+    private bool canStand = false;
+    private bool isInside = false;
+
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -26,26 +29,55 @@ public class PlatformColor : MonoBehaviour
     {
         if (playerColor == platformColor)
         {
-            foreach (Collider2D collider in colliders)
-                collider.isTrigger = false;
-
-            Color temp = spriteRenderer.color;
-            temp.a = 1f;
-            spriteRenderer.color = temp;
+            canStand = true;
+            if(!isInside)
+                MakeStandable();
         }
         else
         {
-            foreach (Collider2D collider in colliders)
-                collider.isTrigger = true;
-
-            Color temp = spriteRenderer.color;
-            temp.a = 0.25f;
-            spriteRenderer.color = temp;
+            canStand = false;
+            MakeUnstandable();
         }
     }
 
     private void OnDestroy()
     {
         EventManager.instance.PlatformColorEvent -= CheckColor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerInfo>() != null)
+            isInside = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerInfo>() == null) return;
+
+        isInside = false;
+
+        if (canStand)
+            MakeStandable();
+    }
+
+    private void MakeStandable()
+    {
+        foreach (Collider2D collider in colliders)
+            collider.isTrigger = false;
+
+        Color temp = spriteRenderer.color;
+        temp.a = 1f;
+        spriteRenderer.color = temp;
+    }
+
+    private void MakeUnstandable()
+    {
+        foreach (Collider2D collider in colliders)
+            collider.isTrigger = true;
+
+        Color temp = spriteRenderer.color;
+        temp.a = 0.25f;
+        spriteRenderer.color = temp;
     }
 }
